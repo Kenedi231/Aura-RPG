74730fac9c63b580dba54ac8f548659dfe6f08fc
Kolya Kalsin <kalsinkolya@gmail.com>
2025-03-05 22:32:07 +0300

    Create AuraAbilitySystemBlueprintFunctionLibrary ✅
    - Create Data Asset for Attributes Info 📑
    - Create AttributeMenuWidgetController 🎮
    - Create pure function to get OverlayWidgetController ✨

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright myself
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
