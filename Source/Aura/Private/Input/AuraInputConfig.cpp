395ee8067c5a125a48227aa0b20fb390ec619788
Kolya Kalsin <kalsinkolya@gmail.com>
2025-03-07 18:03:59 +0300

    Create InputConfigDataAsset ✅
    - Add Inputs for abilities ⌨️
    - Add Enhanced Input ✨

---
+++ Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -0,0 +1,23 @@
+// Copyright myself
+
+
+#include "Input/AuraInputConfig.h"
+
+const UInputAction* UAuraInputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag,
+	bool bLogNotFound) const
+{
+	for (auto& Action : AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag.MatchesTagExact(InputTag))
+		{
+			return Action.InputAction;
+		}
+	}
+
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Cant find AbilityInputAction for InputTag [%s] on InputConfig [%s]"), *InputTag.ToString(), *GetNameSafe(this))
+	}
+
+	return nullptr;
+}
