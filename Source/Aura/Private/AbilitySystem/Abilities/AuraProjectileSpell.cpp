2f301fd0d55f0a6aee1df156aec6aa666ac45332
Kolya Kalsin <kalsinkolya@gmail.com>
2025-03-13 16:20:25 +0300

    Spawning Projectile 🔥
    - Add TipSocket for Spawning Projectiles 🪄

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -2,13 +2,35 @@
 
 
 #include "AbilitySystem/Abilities/AuraProjectileSpell.h"
-#include "Kismet/KismetSystemLibrary.h"
+#include "Actor/AuraProjectile.h"
+#include "Interaction/CombatInterface.h"
 
 void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
                                            const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
                                            const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+ 
+	const bool bIsServer = HasAuthority(&ActivationInfo);
+	if (!bIsServer) return;
 
-	UKismetSystemLibrary::PrintString(this, FString("Activate Ability"), true, true, FLinearColor::Yellow, 3.0f);
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
+	if (!CombatInterface) return;
+	
+	const FVector SocketLocation = CombatInterface->GetCombatSocketLocation();
+	
+	FTransform SpawnTransform;
+	SpawnTransform.SetLocation(SocketLocation);
+	
+	AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(
+		ProjectileClass,
+		SpawnTransform,
+		GetOwningActorFromActorInfo(),
+		Cast<APawn>(GetAvatarActorFromActorInfo()),
+		ESpawnActorCollisionHandlingMethod::AlwaysSpawn
+	);
+
+	// TODO: Give the Projectile a Gameplay Effect
+
+	Projectile->FinishSpawning(SpawnTransform);
 }
