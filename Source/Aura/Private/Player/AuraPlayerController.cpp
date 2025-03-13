d8e205d17281ed15b1e614b78d6ce639e0887006
Kolya Kalsin <kalsinkolya@gmail.com>
2025-03-13 21:15:58 +0300

    Spawning Projectile By Event ✨
    - Create AnimMontage for FireBolt 🪄
    - Create Custom AnimNotify 🔔

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -184,6 +184,7 @@ void AAuraPlayerController::SetupInputComponent()
 
 void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 {
+	bAutoRunning = false;
 	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
 	const FRotator Rotation = GetControlRotation();
 	const FRotator YawRotation(0.0f, Rotation.Yaw, 0.0f);
