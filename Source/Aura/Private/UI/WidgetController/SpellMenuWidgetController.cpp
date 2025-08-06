e628705f57a8b2750e397de41fa9ee1b257ab0ba
Kolya Kalsin <kalsinkolya@gmail.com>
2025-08-06 19:25:04 +0300

    Spell Reassigned ✨

--- Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -159,6 +159,8 @@ void USpellMenuWidgetController::OnAbilityEquipped(const FGameplayTag& AbilityTa
 	AbilityInfoDelegate.Broadcast(Info);
 
 	StopWaitingForEquipSelectionDelegate.Broadcast(Info.AbilityType);
+	SpellGlobeReassignedDelegate.Broadcast(AbilityTag);
+	SpellGlobeDeselected();
 }
 
 void USpellMenuWidgetController::ShouldEnableButtons(const FGameplayTag& AbilityStatus, int32 SpellPoints,
